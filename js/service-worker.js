2c593d201bb77acdc805512ad6b27e18ae783699
Raghu2407 <raghoothamrvk7@gmail.com>
2020-02-05 16:06:57 +0530

    changes

--- js/service-worker.js
+++ js/service-worker.js
@@ -41,3 +41,11 @@ function fetchAndCache(url) {
             // You could return a custom offline 404 page here
         });
 }
+
+//notifications
+self.addEventListener('notificationclose', function (e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+
+    console.log('Closed notification: ' + primaryKey);
+});
