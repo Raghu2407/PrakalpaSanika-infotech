0926299893d80338ea9693621d8c26b636e0d131
Raghu2407 <raghoothamrvk7@gmail.com>
2020-02-05 16:01:14 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -90,3 +90,16 @@ if ('serviceWorker' in navigator) {
             console.log('Registration failed: ', error);
         });
 }
+
+//notification
+Notification.requestPermission(function (status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(function (reg) {
+            reg.showNotification('Hello world!');
+        });
+    }
+}
--- service-worker.js
+++ service-worker.js
@@ -61,15 +61,3 @@ function fetchAndCache(url) {
 }
 
 
-//notification
-Notification.requestPermission(function (status) {
-    console.log('Notification permission status:', status);
-});
-
-function displayNotification() {
-    if (Notification.permission == 'granted') {
-        navigator.serviceWorker.getRegistration().then(function (reg) {
-            reg.showNotification('Hello world!');
-        });
-    }
-}
